feb2a053344fb098918c2362be791245c8ac73bf
Davi <dr4714@gmail.com>
2024-01-31 19:57:03 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.conteiner}>
       <Text style={styles.title}> Gerencie {"\n"} suas plantas {"\n"} de  forma fácil </Text>
       <Image style={styles.image} source={images.home} resizeMethod='resize' resizeMode='cover' />
+      <Text style={styles.subtitle}>Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de lembrar você {"\n"} sempre que precisar.</Text>
     </View>
   );
 }
@@ -16,14 +17,23 @@ const styles = StyleSheet.create ({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 14,
   },
   title: {
     fontSize: 32,
+    color: "#52665A",
+    textAlign: "center",
   },
   image: {
     width: "80%",
     height: 300,
-  }
+  },
+  subtitle: {
+    fontSize: 17,
+    color: "#5C6660",
+    textAlign: "center",
+
+  },
 
 
 })
