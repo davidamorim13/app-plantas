7dce5d0b786ff6caa4c8c269c15427831d509b1b
Davi <dr4714@gmail.com>
2024-02-07 19:34:04 -0300

    atualizando

--- App.tsx
+++ App.tsx
@@ -1,39 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import images from './src/images/image';
+import { NavigationContainer, } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+
+const   Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.conteiner}>
-      <Text style={styles.title}> Gerencie {"\n"} suas plantas {"\n"} de  forma fácil </Text>
-      <Image style={styles.image} source={images.home} resizeMethod='resize' resizeMode='cover' />
-      <Text style={styles.subtitle}>Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de lembrar você {"\n"} sempre que precisar.</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
-const styles = StyleSheet.create ({
-  conteiner: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 14,
-  },
-  title: {
-    fontSize: 32,
-    color: "#52665A",
-    textAlign: "center",
-  },
-  image: {
-    width: "80%",
-    height: 300,
-  },
-  subtitle: {
-    fontSize: 17,
-    color: "#5C6660",
-    textAlign: "center",
-
-  },
-
-
-})
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import images from '../../images/image';
+
+export default function Home() {
+  return (
+    <View style={styles.conteiner}>
+      <Text style={styles.title}> Gerencie {"\n"} suas plantas {"\n"} de  forma fácil </Text>
+      <Image style={styles.image} source={images.home} resizeMethod='resize' resizeMode='cover' />
+      <Text style={styles.subtitle}>Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de lembrar você {"\n"} sempre que precisar.</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.subtitlebutton}> {"\>"} </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+  conteiner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 14,
+  },
+  title: {
+    fontSize: 32,
+    color: "#52665A",
+    textAlign: "center",
+  },
+  image: {
+    width: "80%",
+    height: 300,
+  },
+  subtitle: {
+    fontSize: 17,
+    color: "#5C6660",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#32B768",
+    borderRadius: 16,
+    height: 56,
+    width: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  subtitlebutton: {
+    fontSize: 22,
+    color: "#FFFFFF",
+  }
+
+
+})
