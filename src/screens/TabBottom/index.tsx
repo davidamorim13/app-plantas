127be7caf8dda05ece49071dde8730fccc67a791
Davi <dr4714@gmail.com>
2024-02-21 20:08:46 -0300

    atualizando

--- src/screens/TabBottom/index.tsx
+++ src/screens/TabBottom/index.tsx
@@ -11,11 +11,17 @@ export default function TabBottom () {
     return (
         <Tab.Navigator screenOptions={({ route }: any) => ({
             tabBarIcon: ({focused, color, sizer}: any) => {
-              return <Ionicons name="home"/>
+                let iconName=""
+                if(route.name === "NewPlant"){
+                    iconName="add-outline"
+                }
+                if(route.name === "Plants")
+                    iconName="leaf-outline"
+              return <Ionicons name={iconName} size={24}/>
             },
           })}>
-            <Tab.Screen name="Nova planta" component={NewPlant}/>
-            <Tab.Screen name="Minhas plantinhas" component={Plants}/>
+            <Tab.Screen name="NewPlant" component={NewPlant}/>
+            <Tab.Screen name="Plants" component={Plants}/>
         </Tab.Navigator>
 
     )
